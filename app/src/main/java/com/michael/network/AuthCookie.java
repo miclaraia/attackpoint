fbae1f587f36a1c03dea7315966953a8b446f8c4
Michael Laraia <micheal.laraia@gmail.com>
2015-08-21 14:19:18 -0400

    changed read() to return Map<>

--- app/src/main/java/com/michael/network/AuthCookie.java
+++ app/src/main/java/com/michael/network/AuthCookie.java
@@ -14,6 +14,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
@@ -30,100 +31,113 @@ public class AuthCookie {
     private Preferences prefs = Singleton.getInstance().getPreferences();
 
     public AuthCookie() {
-        read();
+        Log.d(DEBUG_TAG, "Initializing AuthCookie, no constructor");
+        setCookie();
     }
 
     public AuthCookie(Map<String, List<String>> headers) {
+        Log.d(DEBUG_TAG, "Initializing AuthCookie, map constructor");
         setCookie(headers);
     }
 
     public AuthCookie(String cookie) {
-        //TODO check if cookie already exists in preferences
+        Log.d(DEBUG_TAG, "Initializing AuthCookie, string constructor");
         setCookie(cookie);
     }
 
     public AuthCookie(String t, Date e) {
-        //TODO check if cookie already exists in preferences
+        Log.d(DEBUG_TAG, "Initializing AuthCookie, var constructor");
         setCookie(t, e);
     }
 
     // sets cookie given header map
     public void setCookie(Map<String, List<String>> headers) {
+        Log.d(DEBUG_TAG, "setting cookie from map");
         setCookie(parseHeader(headers));
     }
 
     // Sets the objects token and expiration date, assumes right auth cookie was passed
     public void setCookie(String cookie) {
+        Log.d(DEBUG_TAG, "setting cookie from string");
         String token = parseCookie(cookie);
         Date e = parseExpire(cookie);
         setCookie(token, e);
     }
 
+    public void setCookie() {
+        Log.d(DEBUG_TAG, "setting cookie from read()");
+        Map<String, Object> map = read();
+        if (map != null) {
+            String t = (String) map.get("key");
+            Date e = (Date) map.get("expire");
+            setCookie(t, e);
+        }
+    }
+
     //checks if oken and expire are valid and whether it has already expired
     //before setting the object variables
     public void setCookie(String t, Date e) {
-        if (read()) {
-            if (checkVals(t, e) && checkTime(e) && e.compareTo(expire) > 0) {
-                token = t;
-                expire = e;
-                save();
-            }
-        } else if (checkVals(t, e) && checkTime(e)) {
+        Log.d(DEBUG_TAG, "setting cookie from vars");
+        if (checkVals(t, e) && checkTime(e)) {
             token = t;
             expire = e;
             save();
-        } else {
-            token = "";
-            expire = null;
         }
     }
 
     // saves cookie to preferences
     public void save() {
-        if (checkTime(expire)) {
-            try {
-                prefs.saveCookie(this.serialize());
-            } catch (JSONException e) {
-                e.printStackTrace();
-                Toast.makeText(Singleton.getInstance().getContext(),
-                        DEBUG_TAG + "JSONException saving cookie", Toast.LENGTH_LONG).show();
-            }
+        Log.d(DEBUG_TAG, "saving cookie to preferences");
+        try {
+            prefs.saveCookie(this.serialize());
+        } catch (JSONException e) {
+            Log.e(DEBUG_TAG, "JSONException trying to save cookie");
+            e.printStackTrace();
+            Toast.makeText(Singleton.getInstance().getContext(),
+                    DEBUG_TAG + "JSONException saving cookie", Toast.LENGTH_LONG).show();
         }
     }
 
     // reads cookie from preferences
-    private boolean read() {
+    private Map<String, Object> read() {
+        Log.d(DEBUG_TAG, "reading cookie from preferences");
         try {
+            //gets and parses json from preferences
             String cookie = prefs.getCookie();
             JSONObject json = new JSONObject(cookie);
-            String token = (String) json.get("key");
-            String e = (String) json.get("expire");
-            Date expire = parseExpire(e);
-            if (checkTime(expire)) {
-                //setCookie(token, expire);
-                this.token = token;
-                this.expire = expire;
-                return true;
+            String t = (String) json.get("key");
+            Log.v(DEBUG_TAG, "found cookie token: " + t);
+            String ex = (String) json.get("expire");
+            //makes sure existing cookies hasnt expired
+            Date e = parseExpire(ex);
+            if (checkVals(t, e) && checkTime(e)) {
+                Map<String, Object> map = new HashMap<String, Object>();
+                map.put("key", t);
+                map.put("expire", e);
+                return map;
             } else {
                 expire();
-                return false;
+                return null;
             }
         } catch (JSONException e) {
+            Log.e(DEBUG_TAG, "JSONException trying to read cookie");
             e.printStackTrace();
             prefs.removeCookie();
             Toast.makeText(Singleton.getInstance().getContext(),
                     DEBUG_TAG + "JSONException reading cookie", Toast.LENGTH_LONG).show();
-            return false;
+            return null;
         } catch (NullPointerException e) {
+            Log.e(DEBUG_TAG, "NullPointer trying to read cookie");
             e.printStackTrace();
             Toast.makeText(Singleton.getInstance().getContext(),
-                    DEBUG_TAG + "JSONException reading cookie", Toast.LENGTH_LONG).show();
-            return false;
+                    DEBUG_TAG + "NullPointer reading cookie", Toast.LENGTH_LONG).show();
+            return null;
         }
     }
 
     // removes cookie from preferences
     private void expire() {
+        Log.d(DEBUG_TAG, "expiring cookie from preferences");
         prefs.removeCookie();
     }
 
@@ -197,37 +211,6 @@ public class AuthCookie {
         } else return null;
     }
 
-    private int parseMonth(String month) {
-        switch (month) {
-            case "Jan":
-                return 1;
-            case "Feb":
-                return 2;
-            case "Mar":
-                return 3;
-            case "Apr":
-                return 4;
-            case "May":
-                return 5;
-            case "Jun":
-                return 6;
-            case "Jul":
-                return 7;
-            case "Aug":
-                return 8;
-            case "Sep":
-                return 9;
-            case "Oct":
-                return 10;
-            case "Nov":
-                return 11;
-            case "Dec":
-                return 12;
-            default:
-                return 0;
-        }
-    }
-
     //cookie string to be stored in preferences
     private String serialize() throws JSONException {
         JSONObject json = new JSONObject();
@@ -237,7 +220,7 @@ public class AuthCookie {
     }
 
     public String toString() {
-        if (checkTime(this.expire) && checkVals(token, expire)) return key + "=" +  token + ";";
+        if (checkVals(token, expire) && checkTime(this.expire)) return key + "=" +  token + ";";
         return "";
     }
 
