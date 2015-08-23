e3e15d848865de299d9db2889d381d1114da0b96
Michael Laraia <micheal.laraia@gmail.com>
2015-08-23 19:26:22 -0400

    populated layout and fragment class

--- app/src/main/java/com/michael/attackpoint/LogFragment.java
+++ app/src/main/java/com/michael/attackpoint/LogFragment.java
@@ -1,7 +1,21 @@
 package com.michael.attackpoint;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by michael on 8/23/15.
  */
-public class LogFragment {
+public class LogFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.log_fragment, container, false);
+    }
+
 }
