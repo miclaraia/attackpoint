40ff63cad724a09e1f198e25ee08f1c387e696b9
Michael Laraia <micheal.laraia@gmail.com>
2015-08-24 13:35:51 -0400

    populate drawer adapter

--- app/src/main/java/com/michael/attackpoint/adapters/DrawerAdapter.java
+++ app/src/main/java/com/michael/attackpoint/adapters/DrawerAdapter.java
@@ -1,7 +1,41 @@
 package com.michael.attackpoint.adapters;
 
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
 /**
  * Created by michael on 8/24/15.
  */
-public class DrawerAdapter {
+public class DrawerAdapter extends BaseAdapter {
+    private Context context;
+    private ArrayList<String> navDrawerItems;
+
+    public DrawerAdapter(Context context, ArrayList<String> navDrawerItems){
+        this.context = context;
+        this.navDrawerItems = navDrawerItems;
+    }
+
+    @Override
+    public int getCount() {
+        return navDrawerItems.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return navDrawerItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
 }
