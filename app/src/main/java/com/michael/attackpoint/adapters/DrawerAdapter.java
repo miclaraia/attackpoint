14bc29fb73347417e442c56169d0d0fb97029d62
Michael Laraia <micheal.laraia@gmail.com>
2015-08-24 13:48:17 -0400

    wrote getView in DrawerAdapter

--- app/src/main/java/com/michael/attackpoint/adapters/DrawerAdapter.java
+++ app/src/main/java/com/michael/attackpoint/adapters/DrawerAdapter.java
@@ -1,9 +1,16 @@
 package com.michael.attackpoint.adapters;
 
+import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.michael.attackpoint.R;
+import com.michael.objects.DrawerItem;
 
 import java.util.ArrayList;
 
@@ -12,9 +19,9 @@ import java.util.ArrayList;
  */
 public class DrawerAdapter extends BaseAdapter {
     private Context context;
-    private ArrayList<String> navDrawerItems;
+    private ArrayList<DrawerItem> navDrawerItems;
 
-    public DrawerAdapter(Context context, ArrayList<String> navDrawerItems){
+    public DrawerAdapter(Context context, ArrayList<DrawerItem> navDrawerItems){
         this.context = context;
         this.navDrawerItems = navDrawerItems;
     }
@@ -36,6 +43,17 @@ public class DrawerAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.drawer_item, null);
+        }
+
+        ImageView itemIcon = (ImageView) convertView.findViewById(R.id.item_icon);
+        TextView itemName = (TextView) convertView.findViewById(R.id.item_text);
+
+        itemIcon.setImageResource(navDrawerItems.get(position).icon);
+        itemName.setText(navDrawerItems.get(position).name);
+
+        return convertView;
     }
 }
