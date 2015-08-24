5d57b4261ebf31e945930294f98898b97a031384
Michael Laraia <micheal.laraia@gmail.com>
2015-08-24 13:41:58 -0400

    Object to contain drawer data

---
+++ app/src/main/java/com/michael/objects/DrawerItem.java
@@ -0,0 +1,14 @@
+package com.michael.objects;
+
+/**
+ * Created by michael on 8/24/15.
+ */
+public class DrawerItem {
+    public String name;
+    public int icon;
+
+    public DrawerItem(String name, int icon) {
+        this.name = name;
+        this.icon = icon;
+    }
+}
