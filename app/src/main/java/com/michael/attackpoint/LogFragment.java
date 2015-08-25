8acf85ebd2c0d9dc8fd3a54452afc3f1d359d09e
Michael Laraia <micheal.laraia@gmail.com>
2015-08-25 16:23:03 -0400

    recovery post crash

--- app/src/main/java/com/michael/attackpoint/LogFragment.java
+++ app/src/main/java/com/michael/attackpoint/LogFragment.java
@@ -30,7 +30,7 @@ public class LogFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.log_fragment, container,false);
+        View view = inflater.inflate(R.layout.fragment_log, container,false);
         recList = (RecyclerView) view.findViewById(R.id.cardList);
         recList.setLayoutManager(linearLayout);
 
