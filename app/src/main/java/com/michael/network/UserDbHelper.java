c0417a4996aa395f5cde9dbb6a1f36b1f2a86d51
Michael Laraia <micheal.laraia@gmail.com>
2015-10-16 10:14:17 +0200

    added user class

--- app/src/main/java/com/michael/network/UserDbHelper.java
+++ app/src/main/java/com/michael/network/UserDbHelper.java
@@ -1,7 +1,56 @@
 package com.michael.network;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
 /**
  * Created by michael on 10/15/15.
  */
-public class UserDbHelper {
+public class UserDbHelper extends SQLiteOpenHelper {
+
+    public static final String TABLE = "users";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NAME = "user";
+    public static final String COLUMN_TOKEN = "token";
+    public static final String COLUMN_EXPIRE = "expire";
+
+
+    private static final String DATABASE_NAME = "userdata.db";
+    private static final int DATABASE_VERSION = 1;
+
+    // Database creation sql statement
+    private static final String DATABASE_CREATE = "CREATE TABLE "
+            + TABLE + "(" + COLUMN_ID
+            + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_NAME
+            + " TEXT NOT NULL, " + COLUMN_TOKEN
+            + " TEXT NOT NULL, " + COLUMN_EXPIRE
+            + " TEXT NOT NULL);";
+
+    public UserDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(UserDbHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE);
+        onCreate(db);
+    }
+
+    public String[] insertColumns() {
+        String[] columns = new String[3];
+        columns[0] = COLUMN_NAME;
+        columns[1] = COLUMN_TOKEN;
+        columns[2] = COLUMN_EXPIRE;
+        return columns;
+    }
 }
---
+++ app/src/main/java/com/michael/network/Users.java
@@ -0,0 +1,7 @@
+package com.michael.network;
+
+/**
+ * Created by michael on 10/16/15.
+ */
+public class Users {
+}
