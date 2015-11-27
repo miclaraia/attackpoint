31e2d49ade0121af8340c1fd0af899ae1384868c
Michael Laraia <micheal.laraia@gmail.com>
2015-11-27 17:36:33 +0100

    added user db helper

--- app/src/main/java/com/michael/database/UserDBHelper.java
+++ app/src/main/java/com/michael/database/UserDBHelper.java
@@ -6,27 +6,21 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 /**
- * Created by michael on 10/20/15.
+ * Created by michael on 11/27/15.
  */
-public class CookieDBHelper extends SQLiteOpenHelper {
-
+public class UserDBHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "cookies.db";
     private static final int DATABASE_VERSION = 1;
 
-    public CookieDBHelper(Context context) {
+    public UserDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
-
-    public static String selectWhere(String column, String user) {
-        String s = column
-                + "LIKE \"" + user + "\"";
-        return s;
     }
 
     @Override
     public void onCreate(SQLiteDatabase database) {
-        database.execSQL(UserTable.TABLE_CREATE);
+        database.execSQL(CookieTable.TABLE_CREATE);
+        //database.execSQL(UserTable.TABLE_CREATE);
     }
 
     @Override
