7feb19c8c4b51d1e1a9da810a17d0bdf2c8ccee2
Michael Laraia <micheal.laraia@gmail.com>
2016-02-03 15:37:07 +0100

    started adapter for discussion

---
+++ app/src/main/java/com/michael/attackpoint/discussion/Adapter.java
@@ -0,0 +1,16 @@
+package com.michael.attackpoint.discussion;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by michael on 2/3/16.
+ */
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+    
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View v) {
+            super(v);
+        }
+    }
+}
