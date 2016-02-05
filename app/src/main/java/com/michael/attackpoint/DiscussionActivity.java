dcc565b6989b524500e4209ee3ffd10a31c1533e
Michael Laraia <micheal.laraia@gmail.com>
2016-02-05 11:07:00 +0100

    updating adapter

--- app/src/main/java/com/michael/attackpoint/DiscussionActivity.java
+++ app/src/main/java/com/michael/attackpoint/DiscussionActivity.java
@@ -4,14 +4,24 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.michael.attackpoint.discussion.Adapter;
+
 public class DiscussionActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private Adapter mAdapter;
+    private Singleton singleton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        singleton = Singleton.getInstance();
+        singleton.setActivity(this);
+
         setContentView(R.layout.activity_discussion);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -24,6 +34,17 @@ public class DiscussionActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.discussion);
+        LinearLayoutManager linearLayout = new LinearLayoutManager(getActivity());
+        //TODO recyclerview is null becuase not logged in. Something in the view is null?
+        mRecyclerView.setLayoutManager(linearLayout);
+
+        mAdapter = new UsersAdapter(this, new ArrayList<User>());
+        mAdapter.notifyDataSetChanged();
+        getFavorites();
+
+        mRecyclerView.setAdapter(mAdapter);
     }
 
 }
--- app/src/main/java/com/michael/attackpoint/discussion/Adapter.java
+++ app/src/main/java/com/michael/attackpoint/discussion/Adapter.java
@@ -1,9 +1,12 @@
 package com.michael.attackpoint.discussion;
 
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.michael.attackpoint.R;
@@ -19,8 +22,10 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     private static final String DEBUG_TAG = "discussion.A";
 
     private List<Comment> mComments;
+    private Activity mActivity;
 
-    public Adapter(List<Comment> comments) {
+    public Adapter(Activity activity, List<Comment> comments) {
+        mActivity = activity;
         mComments = comments;
     }
 
@@ -54,6 +59,15 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         vh.user.setText(comment.getUser());
         vh.date.setText(comment.getDate());
         vh.content.setText(comment.getText());
+
+        // colors comment background alternating light/dark
+        int color = 0;
+        if ((i & 1) == 0) {
+            color = mActivity.getResources().getColor(R.color.colorCardDialog);
+        } else {
+            color = mActivity.getResources().getColor(R.color.colorBackground);
+        }
+        vh.container.setBackgroundColor(color);
     }
 
     @Override
@@ -74,12 +88,14 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         public TextView user;
         public TextView date;
         public TextView content;
+        public RelativeLayout container;
 
         public ViewHolder(View v) {
             super(v);
             user = (TextView) v.findViewById(R.id.comment_user);
             date = (TextView) v.findViewById(R.id.comment_date);
             content = (TextView) v.findViewById(R.id.comment_content);
+            container = (RelativeLayout) v.findViewById(R.id.comment_container);
         }
     }
 }
