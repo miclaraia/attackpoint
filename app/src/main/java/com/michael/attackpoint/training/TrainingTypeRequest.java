5eefad23103f36c632fff821f7b296a8d8b2abb3
Michael Laraia <micheal.laraia@gmail.com>
2016-03-03 14:13:34 +0100

    added request for activity types

--- app/src/main/java/com/michael/attackpoint/training/TrainingTypeRequest.java
+++ app/src/main/java/com/michael/attackpoint/training/TrainingTypeRequest.java
@@ -3,28 +3,60 @@ package com.michael.attackpoint.training;
 import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Created by michael on 2/26/16.
  */
 public class TrainingTypeRequest extends Request<Map<String, Integer>> {
-    public TrainingTypeRequest(String url, Response.ErrorListener listener) {
-        super(url, listener);
-    }
+    private static final String URL = "http://www.attackpoint.org/newtraining.jsp";
 
-    public TrainingTypeRequest(int method, String url, Response.ErrorListener listener) {
-        super(method, url, listener);
+    private Response.Listener<Map<String, Integer>> mListener;
+
+    public TrainingTypeRequest(int userID, Response.Listener<Map<String, Integer>> listener, Response.ErrorListener errorListener) {
+        super(Method.GET, URL, errorListener);
+        mListener = listener;
     }
 
     @Override
     protected Response<Map<String, Integer>> parseNetworkResponse(NetworkResponse networkResponse) {
-        return null;
+        try {
+            String responseString = new String(networkResponse.data, HttpHeaderParser.parseCharset(networkResponse.headers));
+            Map<String, Integer> response = getMap(Jsoup.parse(responseString));
+            
+            return Response.success(response, HttpHeaderParser.parseCacheHeaders(networkResponse));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return Response.error(new VolleyError(e));
+        }
     }
 
     @Override
-    protected void deliverResponse(Map<String, Integer> stringIntegerMap) {
+    protected void deliverResponse(Map<String, Integer> response) {
+        mListener.onResponse(response);
+    }
 
+    //++++++++++++++++++++   JSOUP   ++++++++++++++++++++
+    private Map<String, Integer> getMap(Document soup) {
+        Element menu = soup.getElementById("activitytypeid");
+        Elements options = menu.getElementsByTag("option");
+        Map<String, Integer> map = new HashMap<>();
+        for (Element option : options) {
+            String key = option.text();
+            Integer value = Integer.parseInt(option.attr("value"));
+            map.put(key, value);
+        }
+
+        return map;
     }
 }
