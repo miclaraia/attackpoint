dab3c8f7a6ef5388e3ae9e06c6c8d73940efd9c1
Michael Laraia <micheal.laraia@gmail.com>
2016-03-11 14:21:11 +0100

    implemented toolbar and coordinatorlayout

--- app/src/main/java/com/michael/attackpoint/MainActivity.java
+++ app/src/main/java/com/michael/attackpoint/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -30,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         Fragment fragment = new UsersFragment();
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
