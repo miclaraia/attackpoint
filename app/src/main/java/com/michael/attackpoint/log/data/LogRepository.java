d76b9a5ebd1947be66939229159be66bb1d72287
Michael Laraia <micheal.laraia@gmail.com>
2016-03-18 18:40:46 +0100

    interfaces for data system

--- app/src/main/java/com/michael/attackpoint/log/data/LogRepository.java
+++ app/src/main/java/com/michael/attackpoint/log/data/LogRepository.java
@@ -1,9 +1,33 @@
 package com.michael.attackpoint.log.data;
 
+import android.support.annotation.NonNull;
+
+import com.michael.attackpoint.log.loginfo.LogInfo;
+
+import java.util.List;
+
 /**
  * Created by michael on 3/16/16.
  */
 public interface LogRepository {
 
+    interface LoadLogCallback {
+        void onLoaded(List<LogInfo> logList);
+    }
 
+    interface LoadLogEntryCallback {
+        void onLoaded(LogInfo logInfo);
+    }
+
+    interface RefreshCallback {
+        void done();
+    }
+
+    void getLog(@NonNull int userID, @NonNull LoadLogCallback callback);
+
+    void getLogEntry(@NonNull int userID, @NonNull int id, @NonNull LoadLogEntryCallback callback);
+
+    void saveLogEntry(@NonNull int userID, @NonNull LogInfo logInfo);
+
+    void refreshData(@NonNull int userID, RefreshCallback callback);
 }
