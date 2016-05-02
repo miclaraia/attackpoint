10e8c935c4cf000720978210bd5b0eb4f90a4677
Michael Laraia <micheal.laraia@gmail.com>
2016-05-02 15:02:44 -0500

    adding local repository for discussions

--- app/src/main/java/com/michael/attackpoint/discussion/data/DiscussionRepository.java
+++ app/src/main/java/com/michael/attackpoint/discussion/data/DiscussionRepository.java
@@ -1,7 +1,31 @@
 package com.michael.attackpoint.discussion.data;
 
+import com.android.volley.VolleyError;
+import com.michael.attackpoint.discussion.Discussion;
+
+import java.util.List;
+
 /**
  * Created by michael on 5/2/16.
  */
-public class DiscussionRepository {
+public interface DiscussionRepository {
+
+    void getDiscussion(boolean forceRefresh, int id, LoadCallback callback);
+
+    void getDiscussion(int id, LoadCallback callback);
+
+    void refreshDiscussion(int id, RefreshCallback callback);
+
+    interface LoadCallback {
+        void onLoaded(Discussion discussion);
+
+        void onError(Exception e);
+    }
+
+    interface RefreshCallback {
+        void onLoaded(Discussion discussion);
+
+        void onError(VolleyError e);
+    }
+
 }
--- app/src/main/java/com/michael/attackpoint/discussion/data/DiscussionRepositoryImpl.java
+++ app/src/main/java/com/michael/attackpoint/discussion/data/DiscussionRepositoryImpl.java
@@ -1,7 +1,100 @@
 package com.michael.attackpoint.discussion.data;
 
+import com.android.volley.NoConnectionError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+import com.michael.attackpoint.discussion.Discussion;
+import com.michael.attackpoint.util.Singleton;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by michael on 5/2/16.
  */
-public class DiscussionRepositoryImpl {
+public class DiscussionRepositoryImpl implements DiscussionRepository {
+
+    protected Map<Integer, Discussion> mDiscussions;
+    protected RequestQueue mRequestQueue;
+
+    public DiscussionRepositoryImpl() {
+        mDiscussions = new HashMap<>(0);
+        mRequestQueue = Volley.newRequestQueue(Singleton.getInstance().getContext());
+    }
+
+    public DiscussionRepositoryImpl(RequestQueue requestQueue) {
+        mDiscussions = new HashMap<>(0);
+        mRequestQueue = requestQueue;
+    }
+
+    @Override
+    public void getDiscussion(final boolean forceRefresh, final int id, final LoadCallback callback) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (forceRefresh || !inLocalCache(id)) {
+                    removeLocalCache(id);
+
+                    refreshDiscussion(id, new RefreshCallback() {
+                        @Override
+                        public void onLoaded(Discussion discussion) {
+                            callback.onLoaded(discussion);
+                        }
+
+                        @Override
+                        public void onError(VolleyError e) {
+                            callback.onError(e);
+                            if (e instanceof NoConnectionError && inLocalCache(id)) {
+                                callback.onLoaded(getLocalCache(id));
+                            }
+                        }
+                    });
+                } else {
+                    callback.onLoaded(getLocalCache(id));
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public void getDiscussion(int id, LoadCallback callback) {
+        getDiscussion(false, id, callback);
+    }
+
+    @Override
+    public void refreshDiscussion(final int id, final RefreshCallback callback) {
+        DiscussionRequest request = new DiscussionRequest(id, new Response.Listener<Discussion>() {
+            @Override
+            public void onResponse(Discussion discussion) {
+                mDiscussions.put(id, discussion);
+                callback.onLoaded(discussion);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError e) {
+                callback.onError(e);
+            }
+        });
+        mRequestQueue.add(request);
+    }
+
+    protected boolean inLocalCache(int id) {
+        if (mDiscussions.containsKey(id)) return true;
+        return false;
+    }
+
+    protected Discussion getLocalCache(int id) {
+        Discussion discussion = mDiscussions.get(id);
+        return discussion;
+    }
+
+    protected void removeLocalCache(int id) {
+        if (inLocalCache(id)) {
+            mDiscussions.remove(id);
+        }
+    }
 }
