cad3fcd8d7e8b9cd0942c8ccdb6b77aaf8a67504
Michael Laraia <micheal.laraia@gmail.com>
2016-05-06 11:21:40 -0500

    test for discussion presenter

---
+++ app/src/test/java/com/michael/attackpoint/discussion/DiscussionPresenterTest.java
@@ -0,0 +1,55 @@
+package com.michael.attackpoint.discussion;
+
+import com.michael.attackpoint.discussion.data.DiscussionRepository;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by michael on 5/6/16.
+ */
+public class DiscussionPresenterTest {
+
+    @Mock
+    private DiscussionRepository mRepository;
+
+    @Mock
+    private DiscussionContract.View mView;
+
+    @Mock
+    private Discussion mDiscussion;
+
+    @Captor
+    private ArgumentCaptor<DiscussionRepository.LoadCallback> mCallbackCaptor;
+
+    private DiscussionPresenter mPresenter;
+
+    @Before
+    public void setup() {
+        mPresenter = new DiscussionPresenter(mRepository, mView, 100);
+    }
+
+    @Test
+    public void loadDiscussion_test() {
+        mPresenter.loadDiscussion();
+
+        verify(mView).setProgressIndicator(true);
+        verify(mRepository).getDiscussion(100, mCallbackCaptor.capture());
+
+        mCallbackCaptor.getValue().onLoaded(mDiscussion);
+
+        verify(mView).setProgressIndicator(false);
+        verify(mView).showDiscussion(mDiscussion);
+    }
+
+    @Test
+    public void newComment_test() {
+        mPresenter.newComment();
+        verify(mView).showNewComment();
+    }
+}
